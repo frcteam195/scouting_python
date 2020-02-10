9a96e937211178720905b6ffebb542cff65e14b3
Munise <b.munise.a@gmail.com>
2020-02-10 18:49:34 -0500

    This is the module for the total balls. Please check and see if it works. Don't want to modify AnalysisIF.py

--- analysisTypes/totalBalls.py
+++ analysisTypes/totalBalls.py
@@ -1,5 +1,4 @@
 import statistics
-import numpy as np
 # ******************** AnalysisTypeID = 3 = Total Balls *******************
 
 def totalBalls(analysis, rsRobotMatches):
@@ -14,21 +13,45 @@ def totalBalls(analysis, rsRobotMatches):
         rsCEA['Team'] = matchResults[analysis.columns.index('Team')]
         rsCEA['EventID'] = matchResults[analysis.columns.index('EventID')]
 
-        ballLow = matchResults[analysis.columns.index('BallLow')]
-        if ballLow is None:
-            ballLow = 0
-        ballOuter = matchResults[analysis.columns.index('AutoBallOuter')]
-        if ballOuter is None:
-            ballOuter = 0
-        ballInner = matchResults[analysis.columns.index('AutoBallInner')]
-        if ballInner is None:
-            ballInner = 0
+        TeleBallLowZone1 = matchResults[analysis.columns.index('TeleBallLowZone1')]
+        if TeleBallLowZone1 is None:
+            TeleBallLowZone1 = 0
+        TeleBallOuterZone1 = matchResults[analysis.columns.index('TeleBallOuterZone1')]
+        if TeleBallOuterZone1 is None:
+            TeleBallOuterZone1 = 0
+        TeleBallInnerZone1 = matchResults[analysis.columns.index('TeleBallInnerZone1')]
+        if TeleBallInnerZone1 is None:
+            TeleBallInnerZone1 = 0
+        TeleBallOuterZone2 = matchResults[analysis.columns.index('TeleBallOuterZone2')]
+        if TeleBallOuterZone2 is None:
+            TeleBallOuterZone2 = 0
+        TeleBallInnerZone2 = matchResults[analysis.columns.index('TeleBallInnerZone2')]
+        if TeleBallInnerZone2 is None:
+            TeleBallInnerZone2 = 0
+        TeleBallOuterZone3 = matchResults[analysis.columns.index('TeleBallOuterZone3')]
+        if TeleBallOuterZone3 is None:
+            TeleBallOuterZone3 = 0
+        TeleBallInnerZone3 = matchResults[analysis.columns.index('TeleBallInnerZone3')]
+        if TeleBallInnerZone3 is None:
+            TeleBallInnerZone3 = 0
+        TeleBallOuterZone4 = matchResults[analysis.columns.index('TeleBallOuterZone4')]
+        if TeleBallOuterZone4 is None:
+            TeleBallOuterZone4 = 0
+        TeleBallInnerZone4 = matchResults[analysis.columns.index('TeleBallInnerZone4')]
+        if TeleBallInnerZone4 is None:
+            TeleBallInnerZone4 = 0
+        TeleBallOuterZone5 = matchResults[analysis.columns.index('TeleBallOuterZone5')]
+        if TeleBallOuterZone5 is None:
+            TeleBallOuterZone5 = 0
+        TeleBallInnerZone5 = matchResults[analysis.columns.index('TeleBallInnerZone5')]
+        if TeleBallInnerZone5 is None:
+            TeleBallInnerZone5 = 0
 
         numberOfMatchesPlayed += 1
-        totalHighBalls = ballOuter + ballInner
-        totalBalls = totalHighBalls + ballLow
-        totalHighBallsList.append(ballOuter + ballInner)
-        totalBallsList.append(ballLow + ballOuter + ballInner)
+        totalHighBalls = TeleBallOuterZone1 + TeleBallInnerZone1 + TeleBallOuterZone2 + TeleBallInnerZone2 + TeleBallOuterZone3 + TeleBallInnerZone3 + TeleBallOuterZone4 + TeleBallInnerZone4 + TeleBallOuterZone5 + TeleBallInnerZone5
+        totalBalls = totalHighBalls + TeleBallLowZone1
+        totalHighBallsList.append(TeleBallOuterZone1 + TeleBallInnerZone1 + TeleBallOuterZone2 + TeleBallInnerZone2 + TeleBallOuterZone3 + TeleBallInnerZone3 + TeleBallOuterZone4 + TeleBallInnerZone4 + TeleBallOuterZone5 + TeleBallInnerZone5)
+        totalBallsList.append(TeleBallOuterZone1 + TeleBallInnerZone1 + TeleBallOuterZone2 + TeleBallInnerZone2 + TeleBallOuterZone3 + TeleBallInnerZone3 + TeleBallOuterZone4 + TeleBallInnerZone4 + TeleBallOuterZone5 + TeleBallInnerZone5 + TeleBallLowZone1)
 
         rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = str(
             totalBalls) + "|" + str(totalHighBalls)
