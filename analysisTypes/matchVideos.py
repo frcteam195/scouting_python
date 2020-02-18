b5d54ca7a57138f10d146cad071f3d114f9a9b62
Chineseisasian <dan.daniel.yu@gmail.com>
2020-02-17 20:12:50 -0500

    This should be the finished code for hopperLoad

---
+++ analysisTypes/matchVideos.py
@@ -0,0 +1,11 @@
+import statistics
+# ******************** AnalysisTypeID = 70 = Match Videos *******************
+
+def matchVideos(analysis, rsRobotMatches):
+    # Initialize the rsCEA record set and define variables specific to this function which lie outside the for loop
+    rsCEA = {}
+    rsCEA['AnalysisTypeID'] = 70
+    numberOfMatchesPlayed = 0
+
+
+    for matchResults in rsRobotMatches:
